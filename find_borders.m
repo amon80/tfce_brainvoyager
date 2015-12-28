a365f25d99933fe8adea7caf3b2fa14a7052a59e
Marco Mecchia <Marco Mecchia>
2015-12-28 16:52:49 +0100

    Bugfix

--- find_borders.m
+++ find_borders.m
@@ -1,16 +1,16 @@
 function [beginx, beginy, beginz, endx, endy, endz] = find_borders(x,y,z, x1, y1, z1)
 
-if x1 - 1 <= 0
+if x1 - 1 < 1
 	beginx = x1;
 else
 	beginx = x1 - 1;
 end
-if y1 - 1 <= 0
+if y1 - 1 < 1
 	beginy = y1;
 else
 	beginy = y1 - 1;
 end
-if z1 - 1 <= 0
+if z1 - 1 < 1
 	beginz = z1;
 else
 	beginz = z1 - 1;

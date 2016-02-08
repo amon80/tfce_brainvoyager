c5f9453e389b87ebec1286a3df4781566ba8fb1e
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-02-08 17:18:04 +0100

    Tested succesfully transformation between indices

--- CPorting/main.c
+++ CPorting/main.c
@@ -6,20 +6,16 @@ int main(int argc, char *argv[])
 {
 	int i, j, k;
 	int x, y ,z;
+	int index;
 	float matrix [3][3][3];
 	float map[27];
-	for (i = 0; i < 3; ++i) {
+	for (k = 0; k < 3; ++k) {
 		for (j = 0; j < 3; ++j) {
-			for (k = 0; k < 3; ++k) {
+			for (i = 0; i < 3; ++i) {
 				matrix[i][j][k] = rand()%10;
+				printf("%d\n", linearIndexFromCoordinate(i,j,k,3,3));
 			}
 		}
 	}
-
-	for (i = 0; i < 27; ++i) {
-		coordinatesFromLinearIndex(i, 3, 3, &x, &y, &z);
-		map[i] = matrix[x][y][z];
-	}	
-
 	return 0;
 }

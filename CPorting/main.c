9b5fbf92ed0dcb534fe1cc96c0e8ab86235b5057
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-02-10 16:18:11 +0100

    Adjusted size for tests

--- CPorting/main.c
+++ CPorting/main.c
@@ -1,4 +1,3 @@
-#include "Utilities.h"
 #include "Tfce.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,25 +7,15 @@ int main(int argc, char *argv[])
 	int i, j, k;
 	int x, y ,z;
 	int index;
-	float matrix [20][20][20];
-	float map[30];
+	float map[27];
 
-	for (int i =0; i<30; i++){
-		map[i] = rand()%10;
+	for (int i =0; i<27; i++){
+		map[i] = 1+rand()%10;
 		printf("%f\n",map[i]);
 	}
 
-	for (k = 0; k < 3; ++k) {
-		for (j = 0; j < 3; ++j) {
-			for (i = 0; i < 3; ++i) {
-				matrix[i][j][k] = rand()%10;
-				//printf("%d\n", linearIndexFromCoordinate(i,j,k,3,3));
-			}
-		}
-	}
-
-	float *tfce = tfce_score(map,20,20,20,1,1,1);
-	for (int i =0; i<30; i++){
+	float *tfce = tfce_score(map,3,3,3,2,1,0.1);
+	for (int i =0; i<27; i++){
 		printf("%f\n",tfce[i]);
 	}
 	return 0;
